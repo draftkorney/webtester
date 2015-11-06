059f596fab971816b37f07abe292d14ac31a19bf
Alex <alexandr.korneyko@gmail.com>
2015-11-06 22:16:27 +0200

    add dao

--- src/main/java/ua/alex/source/webtester/dao/impl/AbstractEntityDao.java
+++ src/main/java/ua/alex/source/webtester/dao/impl/AbstractEntityDao.java
@@ -3,9 +3,10 @@ package ua.alex.source.webtester.dao.impl;
 import ua.alex.source.webtester.dao.IEntityDao;
 import ua.alex.source.webtester.entities.IEntity;
 
-public abstract class AbstractEntityDao<T> implements IEntityDao {
+public abstract class AbstractEntityDao<T> implements IEntityDao<T> {
+
     @Override
-    public void save(Object entity) {
+    public void save(T entity) {
         System.out.println("SAVE");
     }
 }
